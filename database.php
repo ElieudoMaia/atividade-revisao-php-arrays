a6de8f61951c9b32b6407917bf720ddc0cbf8658
Elieudo Maia <elieudo.maia@gmail.com>
2021-01-24 17:57:25 -0300

    mudança nos dados dos produtos

--- database.php
+++ database.php
@@ -6,22 +6,22 @@ $menu = [
     'photoURL' => 'calabresa.jpg',
     'price' => '25.70',
     'ingredients' => 'Calabresa,cebola, presunto',
-    'description' => 'Pizza mais ou menos'
+    'description' => 'Pizza mais vendida'
   ],
   [
     'id' => '2',
     'name' => 'Mista',
     'photoURL' => 'mista.jpg',
     'price' => '23.70',
-    'ingredients' => 'Calabresa,cebola, presunto',
-    'description' => 'Pizza mais ou menos'
+    'ingredients' => 'Presunto, queijo,cebola, presunto',
+    'description' => 'Abundante em presunto'
   ],
   [
     'id' => '3',
     'name' => 'Frango',
     'photoURL' => 'frango.jfif',
     'price' => '21.70',
-    'ingredients' => 'Calabresa,cebola, presunto',
-    'description' => 'Pizza mais ou menos'
+    'ingredients' => 'Frango, maionese',
+    'description' => 'Nossa pizza fitness'
   ]
 ];
